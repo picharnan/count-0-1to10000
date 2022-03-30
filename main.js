1d385739ac42d971997489eaf2cf1e8c97a5c404
Picharnan Buaporn <picharnan@live.com>
2022-03-30 10:12:04 +0700

    Remove start stop

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 // จากโพสนี้
 // https://www.facebook.com/photo?fbid=4884625234953716&set=gm.845693799563485
 
-const start = 1;
-const stop = 10000;
 
 //Method 1
 let countZero1 = 0;
-for(let i = start; i <= 10000; i++){
+for(let i = 1; i <= 10000; i++){
     countZero1 += i.toString().replace(/[1-9]/g,"").length;
 }
 console.log(countZero1);
