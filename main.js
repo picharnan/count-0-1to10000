eff5cc68c9e1f30030b42b762a7ff723d09fc907
Picharnan Buaporn <picharnan@live.com>
2022-03-30 10:03:08 +0700

    Got 2 method

---
+++ main.js
@@ -0,0 +1,19 @@
+// จากโพสนี้
+// https://www.facebook.com/photo?fbid=4884625234953716&set=gm.845693799563485
+
+const start = 1;
+const stop = 10000;
+
+//Method 1
+let countZero1 = 0;
+for(let i = start; i <= 10000; i++){
+    countZero1 += i.toString().replace(/[1-9]/g,"").length;
+}
+console.log(countZero1);
+
+
+//Method 2
+const numbers = Array.from({length: 10000}, (_, i) => (i+1).toString());
+const countZero2 = numbers.join("").replace(/[1-9]/g,"").length;
+
+// Answer is 2893
